b43bb729a792c7e83c678eef34eac9fb1f597f54
unknown <anhelinapavliuk@gmail.com>
2023-12-03 23:52:19 +0100

    vanilla js

---
+++ tictactoe.js
@@ -0,0 +1,84 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let runnig = false;
+
+initializeGame();
+
+function initializeGame() {
+  cells.forEach(cell => cell.addEventListener("click", cellClicked));
+  restartBtn.addEventListener("click", restartGame);
+  statusText.textContent = `${currentPlayer}'s turn`;
+  runnig = true;
+}
+
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
+
+  if (options[cellIndex] != "" || !runnig) {
+    return;
+  }
+
+  updateCell(this, cellIndex);
+  checkWinner();
+}
+
+function updateCell(cell, index) {
+  options[index] = changePlayer;
+  cell.textContent = currentPlayer;
+}
+
+function changePlayer() {
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
+    }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins`;
+    runnig = false;
+  } else if (!options.includes("")) {
+    statusText.textContent = `Draw!`;
+    runnig = false;
+  } else {
+    changePlayer();
+  }
+}
+
+function restartGame() {
+  options = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "X";
+  statusText.textContent = `${currentPlayer}'s turn'`;
+  cells.forEach(cell => cell.textContent = "");
+  runnig = true;
+}
